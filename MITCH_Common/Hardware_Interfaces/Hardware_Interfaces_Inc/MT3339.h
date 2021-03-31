cc4cbdc0ab56a140d273cd84f50e4ab6c024bac7
thekamakaji <43650594+thekamakaji@users.noreply.github.com>
2021-03-30 21:25:30 -0400

    Reconciled MT3339 Header

--- MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Inc/MT3339.h
+++ MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Inc/MT3339.h
@@ -1,5 +1,5 @@
 #include "generic_interface.h"
 
 genericSensor_t MT3339_init(UART_HandleTypeDef *huart);
-HAL_StatusTypeDef MT3339_read(genericSensor_t* sensor);
+uint8_t MT3339_read(volatile genericSensor_t* sensor);
 HAL_StatusTypeDef MT3339_receive(genericSensor_t* sensor,uint8_t* buffer);
