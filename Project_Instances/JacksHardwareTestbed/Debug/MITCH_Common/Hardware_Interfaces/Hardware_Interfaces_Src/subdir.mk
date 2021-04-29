677b043a82504e15b572ec5f5888e6042cdd065d
jlwiley100 <77595459+jlwiley100@users.noreply.github.com>
2021-04-29 19:34:08 -0400

    updated ICM 2094

--- Project_Instances/JacksHardwareTestbed/Debug/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/subdir.mk
+++ Project_Instances/JacksHardwareTestbed/Debug/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/subdir.mk
@@ -6,6 +6,7 @@
 C_SRCS += \
 /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/Adafruit_GPS.c \
 /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM20948.c \
+/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM_20948_C.c \
 /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/LED.c \
 /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/MS5607.c \
 /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/MT3339.c \
@@ -15,6 +16,7 @@ C_SRCS += \
 OBJS += \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/Adafruit_GPS.o \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM20948.o \
+./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM_20948_C.o \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/LED.o \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/MS5607.o \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/MT3339.o \
@@ -24,6 +26,7 @@ OBJS += \
 C_DEPS += \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/Adafruit_GPS.d \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM20948.d \
+./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM_20948_C.d \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/LED.d \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/MS5607.d \
 ./MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/MT3339.d \
@@ -36,6 +39,8 @@ MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/Adafruit_GPS.o: /Users/
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F411xE -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Inc" -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Software/Acquisition/Acquisition_Inc" -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/System/System_Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/Adafruit_GPS.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM20948.o: /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM20948.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F411xE -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Inc" -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Software/Acquisition/Acquisition_Inc" -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/System/System_Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM20948.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM_20948_C.o: /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM_20948_C.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F411xE -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Inc" -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Software/Acquisition/Acquisition_Inc" -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/System/System_Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/ICM_20948_C.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/LED.o: /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/LED.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F411xE -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Inc" -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Software/Acquisition/Acquisition_Inc" -I"/Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/System/System_Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/LED.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/MS5607.o: /Users/jackwiley/Documents/GitHub/MITCH/MITCH_Common/Hardware_Interfaces/Hardware_Interfaces_Src/MS5607.c
